b0f0e4ba017584f4736ade31fd85a47efdab6293
SmallAirplaneX <2321764325@qq.com>
2023-10-02 03:29:10 +0800

    对比全量安装插件后版本

--- build/plugin/index.js
+++ build/plugin/index.js
@@ -5,6 +5,11 @@ import vue from '@vitejs/plugin-vue'
  * usage: <script setup name="MyComp"></script>
  */
 import VueSetupExtend from 'vite-plugin-vue-setup-extend'
+
+// rollup打包分析插件
+import visualizer from 'rollup-plugin-visualizer'
+
+import { configHtmlPlugin } from './html'
 import { unocss } from './unocss'
 
 /**
@@ -13,20 +18,33 @@ import { unocss } from './unocss'
  */
 import Components from 'unplugin-vue-components/vite'
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+
 import { configMockPlugin } from './mock'
 
 export function createVitePlugins(viteEnv, isBuild) {
   const plugins = [
     vue(),
     VueSetupExtend(),
+    configHtmlPlugin(viteEnv, isBuild),
     unocss(),
     Components({
       resolvers: [NaiveUiResolver()],
     }),
-    
   ]
+
+  if (isBuild) {
+    plugins.push(
+      visualizer({
+        open: true,
+        gzipSize: true,
+        brotliSize: true,
+      })
+    )
+  }
+
   if (viteEnv?.VITE_APP_USE_MOCK) {
     plugins.push(configMockPlugin(isBuild))
   }
+
   return plugins
 }
